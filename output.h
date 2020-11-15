ff490fdfda0fb0ba2c4a4814cb3b6619081a9690
Christian Garbs <mitch@cgarbs.de>
2020-11-15 16:49:34 +0100

    refactor: combine struct and typedef

--- output.h
+++ output.h
@@ -7,13 +7,11 @@ typedef int     (*sound_open_fn )(void);
 typedef ssize_t (*sound_write_fn)(const void *buf, size_t count);
 typedef int     (*sound_close_fn)(void);
 
-struct _sound_output {
+typedef const struct {
 	sound_open_fn  open;
 	sound_write_fn write;
 	sound_close_fn close;
-};
-
-typedef const struct _sound_output sound_output;
+} sound_output;
 
 sound_output* get_sound_output();
 

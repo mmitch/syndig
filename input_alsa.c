3110b81f576f231f5ad1144d2870a06513237913
Christian Garbs <mitch@cgarbs.de>
2020-11-15 20:56:42 +0100

    switch from ALSA rawmidi to ALSA sequencer
    - easier parsing of MIDI events
    - allow custom name of ALSA MIDI client

--- input_alsa.c
+++ input_alsa.c
@@ -4,12 +4,11 @@
 
 #include <alsa/asoundlib.h>
 
+#include "common.h"
 #include "input.h"
 
-static snd_rawmidi_t *midi_in = NULL;
-
-static uint8_t buffer[1024];
-static const size_t buffer_size = sizeof(buffer);
+static snd_seq_t *sequencer;
+static int port;
 
 static const midi_event NOTE_ON  = { .onoff = true  };
 static const midi_event NOTE_OFF = { .onoff = false };
@@ -18,51 +17,80 @@ static int alsa_open()
 {
 	int err;
 	
-	if ((err = snd_rawmidi_open(&midi_in, NULL, "virtual", SND_RAWMIDI_NONBLOCK)) != 0) {
-		fprintf(stderr, "alsa: could not open MIDI device: %s\n", snd_strerror(err));
+	if ((err = snd_seq_open(&sequencer, "default", SND_SEQ_OPEN_INPUT, SND_SEQ_NONBLOCK)) != 0) {
+		fprintf(stderr, "alsa: could not open sequencer for input: %s\n", snd_strerror(err));
 		return 1;
 	}
+
+	snd_seq_set_client_name(sequencer, PROGRAM_NAME);
+
+	snd_seq_set_client_event_filter(sequencer, SND_SEQ_EVENT_NOTEON);
+	snd_seq_set_client_event_filter(sequencer, SND_SEQ_EVENT_NOTEOFF);
+
+	port = snd_seq_create_simple_port(sequencer, "Input",
+					  SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE,
+					  SND_SEQ_PORT_TYPE_MIDI_GENERIC);
+	if (port < 0) {
+		fprintf(stderr, "alsa: could not open sequencer port: %s\n", snd_strerror(port));
+		return 1;
+	}
+
+	snd_seq_addr_t destination;
+	destination.client = snd_seq_client_id(sequencer);
+	destination.port = port;
+
+	snd_seq_port_subscribe_t *subscriptions;
+	snd_seq_port_subscribe_alloca(&subscriptions);
+	snd_seq_port_subscribe_set_dest(subscriptions, &destination);
+	snd_seq_subscribe_port(sequencer, subscriptions);
 
 	return 0;
 }
 
 static midi_event* alsa_read()
 {
-	ssize_t status;
+	int bytes_left;
+	snd_seq_event_t *event;
 
-	// status combines both error value (when negative) and bytes read (zero or positive)
-	// the ALSA API documentation does not describe the return value at all:
-	// https://www.alsa-project.org/alsa-doc/alsa-lib/group___raw_midi.html#ga9b0952fc04facda83cdf1521325ca475
-	status = snd_rawmidi_read(midi_in, &buffer, buffer_size);
-	if (status < 0 && status != -EBUSY && status != -EAGAIN) {
-		fprintf(stderr, "alsa: MIDI read error: %s\n", snd_strerror(status));
+	bytes_left = snd_seq_event_input(sequencer, &event);
+
+	if (bytes_left == -EAGAIN) {
 		return NULL;
 	}
 
-	if (status == 0) {
-		// no data available
+	if (bytes_left == -ENOSPC) {
+		fprintf(stderr, "alsa: dropped some MIDI input events\n");
 		return NULL;
 	}
+
+	// FIXME: this listens on all MIDI channels
+	// all types: https://www.alsa-project.org/alsa-doc/alsa-lib/group___seq_events.html#gaef39e1f267006faf7abc91c3cb32ea40
+	switch (event->type) {
+
+	case SND_SEQ_EVENT_NOTEON:
+		return &NOTE_ON;
 	
-	// we only care about note on/note off (3 bytes)
-	if (status == 3) {
-		// FIXME: this simply assumes that a new MIDI packet is aligned with the buffer
-		// FIXME: this listens on all MIDI channels
-		uint8_t msb_nibble = buffer[0] & 0xF0;
-		//  uint8_t lsb_nibble = buffer[0] & 0x0F; // LSB == MIDI Channel
-		if (msb_nibble == 0x90) {
-			return &NOTE_ON;
-		}
-		if (msb_nibble == 0x80) {
-			return &NOTE_OFF;
-		}
+	case SND_SEQ_EVENT_NOTEOFF:
+		return &NOTE_OFF;
 	}
+
 	return NULL;
 }
 
 static int alsa_close()
 {
-	snd_rawmidi_close(midi_in);
+	int err;
+
+	if ((err = snd_seq_delete_simple_port(sequencer, port)) != 0) {
+		fprintf(stderr, "alsa: could not close sequencer port: %s\n", snd_strerror(err));
+		return 0;
+	}
+
+	if ((err = snd_seq_close(sequencer)) != 0) {
+		fprintf(stderr, "alsa: could not close sequencer: %s\n", snd_strerror(err));
+		return 0;
+	}
+
 	return 0;
 }
 

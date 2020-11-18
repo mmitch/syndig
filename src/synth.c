ec3dd704f6605514cb2475cfcef1f883ea7225a5
Christian Garbs <mitch@cgarbs.de>
2020-11-18 21:04:59 +0100

    add status output

--- src/synth.c
+++ src/synth.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "common.h"
 #include "midi.h"
 #include "oscillator.h"
 
 int main() {
+	printf("%s is starting\n", PROGRAM_NAME);
+
 	sound_output *sound = NULL;
 	sound = get_sound_output();
 	if (sound->open() != 0) {
 		goto SHUTDOWN;
 	}
+	puts("sound output opened");
 
 	midi_input *midi  = NULL;
 	midi = get_midi_input();
 	if (midi->open() != 0) {
 		goto SHUTDOWN;
 	}
+	puts("MIDI input opened");
 
 	while(1) {
 		receive_midi(midi);
@@ -25,10 +30,12 @@ int main() {
 SHUTDOWN:
 	if (midi != NULL) {
 		midi->close();
+		puts("MIDI input closed");
 	}
 
 	if (sound != NULL) {
 		sound->close();
+		puts("sound output closed");
 	}
 
 	return 0;

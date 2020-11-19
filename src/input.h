0fed440f9a119e6cc1af9a60244269b77b907d05
Christian Garbs <mitch@cgarbs.de>
2020-11-19 20:32:20 +0100

    refactor: use anonymous structs in typedefs

--- src/input.h
+++ src/input.h
@@ -5,25 +5,21 @@
 #include <stdint.h>
 #include <unistd.h>
 
-struct _midi_event {
+typedef struct {
 	bool onoff;
 	uint8_t note;
 	uint8_t velocity;
-};
-
-typedef struct _midi_event midi_event;
+} midi_event;
 
 typedef int         (*midi_open_fn )(void);
 typedef midi_event* (*midi_read_fn )(void);
 typedef int         (*midi_close_fn)(void);
 
-struct _midi_input {
+typedef struct {
 	midi_open_fn  open;
 	midi_read_fn  read;
 	midi_close_fn close;
-};
-
-typedef const struct _midi_input midi_input;
+} midi_input;
 
 midi_input* get_midi_input();
 

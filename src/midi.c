fae13b31cebe16f575482847eb679aac3b95e8ad
Christian Garbs <mitch@cgarbs.de>
2020-11-20 10:34:27 +0100

    support legato
    Only react to NOTE OFF if it matches the currently played note.
    Last NOTE ON wins.

--- src/midi.c
+++ src/midi.c
@@ -9,18 +9,23 @@ static oscillator_type program_map[256] = {
 	SAW_UP,
 };
 
+uint8_t note_playing = 255; // invalid note
+
 void receive_midi(midi_input *midi) {
 	midi_event *event;
 	while ((event = midi->read()) != NULL) {
 		switch (event->type) {
 
 		case NOTE_ON:
-			set_oscillator_frequency(hertz[event->data.note_on.note]);
+			note_playing = event->data.note_on.note;
+			set_oscillator_frequency(hertz[note_playing]);
 			trigger_envelope(event->data.note_on.velocity / MAX_MIDI);
 			break;
 
 		case NOTE_OFF:
-			release_envelope();
+			if (event->data.note_on.note == note_playing) {
+				release_envelope();
+			}
 			break;
 
 		case PROGRAM_CHANGE:

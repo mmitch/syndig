9209f72c6da210f1a6efeafa30540270b781d92b
Christian Garbs <mitch@cgarbs.de>
2020-11-24 19:40:32 +0100

    add copyright information to source files

--- src/output_pulse.c
+++ src/output_pulse.c
@@ -1,3 +1,25 @@
+/*
+ * output_pulse.c - PulseAudio sound driver
+ *
+ * Copyright (C) 2020  Christian Garbs <mitch@cgarbs.de>
+ * Licensed under GNU GPL v3 (or later)
+ *
+ * This file is part of syndig, a simple software synthesizer written in C.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 

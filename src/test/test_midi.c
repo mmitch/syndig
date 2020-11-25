da35951312ea34ebc7a1546235d2348dc01e5581
Christian Garbs <mitch@cgarbs.de>
2020-11-25 21:44:37 +0100

    add tests

--- src/test/test_midi.c
+++ src/test/test_midi.c
@@ -92,17 +92,124 @@ TEST note_on_is_passed_to_polyphony() {
 	event.data.note_on.velocity = 90;
 
 	send_fake_event(&event);
-	
+
 	// when
 	receive_midi(&midi);
 
 	// then
-	ASSERT_EQ(2, read_midi_fake.call_count);
+	ASSERT_EQ(2,  read_midi_fake.call_count);
 
-	ASSERT_EQ(1, play_note_fake.call_count);
+	ASSERT_EQ(1,  play_note_fake.call_count);
 	ASSERT_EQ(37, play_note_fake.arg0_val);
 	ASSERT_IN_RANGE(90 / MAX_MIDI, play_note_fake.arg1_val, 0.0001);
-	
+
+	PASS();
+}
+
+TEST note_off_is_passed_to_polyphony() {
+	// given
+	setup();
+
+	event.type = NOTE_OFF;
+	event.data.note_off.note = 76;
+
+	send_fake_event(&event);
+
+	// when
+	receive_midi(&midi);
+
+	// then
+	ASSERT_EQ(2,  read_midi_fake.call_count);
+
+	ASSERT_EQ(1,  stop_note_fake.call_count);
+	ASSERT_EQ(76, stop_note_fake.arg0_val);
+
+	PASS();
+}
+
+TEST program_change_is_passed_to_oscillator() {
+	// given
+	setup();
+
+	event.type = PROGRAM_CHANGE;
+	event.data.program_change.program = 2;
+
+	send_fake_event(&event);
+
+	// when
+	receive_midi(&midi);
+
+	// then
+	ASSERT_EQ(2,      read_midi_fake.call_count);
+
+	ASSERT_EQ(1,      change_oscillator_type_fake.call_count);
+	ASSERT_EQ(SAW_UP, change_oscillator_type_fake.arg0_val);
+
+	PASS();
+}
+
+TEST program_change_unmapped_values_map_to_square() {
+	// given
+	setup();
+
+	event.type = PROGRAM_CHANGE;
+	event.data.program_change.program = 99;
+
+	send_fake_event(&event);
+
+	// when
+	receive_midi(&midi);
+
+	// then
+	ASSERT_EQ(2,      read_midi_fake.call_count);
+
+	ASSERT_EQ(1,      change_oscillator_type_fake.call_count);
+	ASSERT_EQ(SQUARE, change_oscillator_type_fake.arg0_val);
+
+	PASS();
+}
+
+TEST controller_3_sets_polyphony_mode() {
+	// given
+	setup();
+
+	event.type = CONTROL_CHANGE;
+	event.data.control_change.param = 3;
+	event.data.control_change.value = 3;
+
+	send_fake_event(&event);
+
+	// when
+	receive_midi(&midi);
+
+	// then
+	ASSERT_EQ(2,           read_midi_fake.call_count);
+
+	ASSERT_EQ(1,           set_polyphony_mode_fake.call_count);
+	ASSERT_EQ(ROUND_ROBIN, set_polyphony_mode_fake.arg0_val.mode);
+
+	PASS();
+}
+
+TEST controller_3_unmapped_values_map_to_kill_oldest() {
+	// given
+	setup();
+
+	event.type = CONTROL_CHANGE;
+	event.data.control_change.param = 3;
+	event.data.control_change.value = 99;
+
+	send_fake_event(&event);
+
+	// when
+	receive_midi(&midi);
+
+	// then
+	ASSERT_EQ(2,           read_midi_fake.call_count);
+
+	ASSERT_EQ(1,           set_polyphony_mode_fake.call_count);
+	ASSERT_EQ(KILL_OLDEST, set_polyphony_mode_fake.arg0_val.mode);
+
 	PASS();
 }
 
@@ -113,6 +220,11 @@ int main(int argc, char **argv) {
 
 	RUN_TEST(empty_event_does_nothing);
 	RUN_TEST(note_on_is_passed_to_polyphony);
+	RUN_TEST(note_off_is_passed_to_polyphony);
+	RUN_TEST(program_change_is_passed_to_oscillator);
+	RUN_TEST(program_change_unmapped_values_map_to_square);
+	RUN_TEST(controller_3_sets_polyphony_mode);
+	RUN_TEST(controller_3_unmapped_values_map_to_kill_oldest);
 
 	GREATEST_MAIN_END();
 }
